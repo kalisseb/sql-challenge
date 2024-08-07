26622c29b22b084985346acb109f50c3b6f5c07b
kalisseb <150081638+kalisseb@users.noreply.github.com>
2024-08-07 10:19:45 -0600

    update/upload
    Updating my analysis file with additional sql. Also uploading ERD and schema.sql

--- EmployeeSQL/Data Analysis.sql
+++ EmployeeSQL/Data Analysis.sql
@@ -8,6 +8,40 @@ FROM employees
 WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 
 
-SELECT dept_no, dept_name, emp_no,
+SELECT d.dept_no, d.dept_name, e.emp_no, e.last_name, e.first_name
 FROM departments d
-JOIN dept_manager dm ON d.dept_no
+JOIN dept_manager dm ON d.dept_no = dm.dept_no
+JOIN employees e ON dm.emp_no = e.emp_no;
+
+
+SELECT d.dept_no, de.emp_no, e.last_name, e.first_name, d.dept_name
+FROM departments d
+JOIN dept_emp de ON d.dept_no = de.dept_no
+JOIN employees e ON de.emp_no = e.emp_no;
+
+
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules'
+	AND last_name LIKE 'B%';
+
+
+SELECT e.emp_no, e.last_name, e.first_name
+FROM departments d
+JOIN dept_emp de ON d.dept_no = de.dept_no
+JOIN employees e ON de.emp_no = e.emp_no
+WHERE d.dept_name = 'Sales';
+
+
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM departments d
+JOIN dept_emp de ON d.dept_no = de.dept_no
+JOIN employees e ON de.emp_no = e.emp_no
+WHERE d.dept_name = 'Sales' 
+	OR d.dept_name = 'Development';
+
+
+SELECT last_name, count(emp_no) AS frequency
+FROM employees
+GROUP BY last_name
+ORDER BY frequency DESC;
